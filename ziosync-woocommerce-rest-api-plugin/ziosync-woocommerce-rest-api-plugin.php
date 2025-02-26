1d3caeaedd176d28ea8fc6221fc6cb26f74c4833
Finn <finn@Incogniton.com>
2025-02-26 13:32:14 +0100

    Add modified_after filter

--- ziosync-woocommerce-rest-api-plugin/ziosync-woocommerce-rest-api-plugin.php
+++ ziosync-woocommerce-rest-api-plugin/ziosync-woocommerce-rest-api-plugin.php
@@ -28,6 +28,7 @@
 
     add_action('rest_api_init', 'ZioSync\ZioSync::init');
 
+
     function ziosync_no_woocommerce_notice(){
         ?>
             <div class="error notice">
@@ -35,3 +36,36 @@
             </div>
         <?php
     }
+
+    /**
+     * ! CUSTOM FILTERS
+     */
+    // Filter to add modified_after filter to the listing of all customers endpoints
+    function add_modified_after_filter_to_rest_api($prepared_args, $request)
+    {
+        if($request->get_param('modified_after')){
+            $prepared_args['meta_query'] = array(
+                array(
+                    'key'     => 'last_update',
+                    'value'   => (int) strtotime($request->get_param('modified_after')),
+                    'compare' => '>=',
+                ),
+            );
+        }
+
+        // Allow sorting by modified_date
+        if ($request->get_param('orderby') === 'modified_date') {
+            $prepared_args['orderby'] = 'meta_value';
+            $prepared_args['meta_key'] = 'last_update'; // Ensure the key exists in meta
+        }
+
+        return $prepared_args;
+    }
+    add_filter('woocommerce_rest_customer_query', 'ZioSync\add_modified_after_filter_to_rest_api', 10, 2);
+
+    // Allow 'modified_date' as a valid orderby value
+    function add_custom_orderby_to_rest_api($orderby_values) {
+        $orderby_values[] = 'modified_date';
+        return $orderby_values;
+    }
+    add_filter('woocommerce_rest_customers_collection_params', 'ZioSync\add_custom_orderby_to_rest_api');
